93fd8dbf85315681781f01947e6328aa4b1e52dc
Kasun Dinesh Madusanke <kasundinesh5566@gmail.com>
2025-10-23 14:11:56 +0530

    Initial commit

---
+++ src/main/java/com/example/samplecrudwithauth/SampleCrudWithAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.samplecrudwithauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleCrudWithAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleCrudWithAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/samplecrudwithauth/SampleCrudWithAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samplecrudwithauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleCrudWithAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
